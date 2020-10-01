b895d01584994e70fb1b6200c4425ac2800c3bc8
hugh <270222838@qq.com>
2020-10-01 21:03:05 +0800

    初步实现搜索功能和添加网站功能

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
-console.log('hi')
+const hashMap = [{
+    logo: 'A',
+    url: 'https://www.acfun.cn'
+  },
+  {
+    logo: 'B',
+    url: 'https://www.bilibili.com'
+  }
+]
+$('.addButton')
+  .on('click', () => {
+    let url = window.prompt('请问你要添加的网址是啥？')
+    if (url.indexOf('http') !== 0) {
+      url = 'https://' + url
+    }
+    const $siteList = $('.siteList')
+    const $lastLi = $siteList.find('li.addButton')
+    const $li = $(`<li>
+    <a href="${url}">
+      <div class="site">
+        <div class="logo">${url[0]}</div>
+        <div class="link">${url}</div>
+      </div>
+    </a>
+    <div>
+  </li>`).insertBefore($lastLi)
+  })
