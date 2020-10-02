26b5bc0ce42f5522c92e128928caa1949edb091c
hugh <270222838@qq.com>
2020-10-02 13:47:19 +0800

    实现手机页面功能

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-const hashMap = [{
+const $siteList = $('.siteList')
+const $lastLi = $siteList.find('li.addButton')
+const x = localStorage.getItem('x')
+const xObject = JSON.parse(x)
+const hashMap = xObject || [{
     logo: 'A',
     url: 'https://www.acfun.cn'
   },
@@ -7,21 +11,50 @@ const hashMap = [{
     url: 'https://www.bilibili.com'
   }
 ]
+
+const simplifyUrl = (url) => {
+  return url.replace('https://', '').replace('http://', '').replace('www.', '').replace(/\/.*/, '') //删除/开头的内容
+}
+
+const render = () => {
+  $siteList.find('li:not(.addButton)').remove()
+  hashMap.forEach(node => {
+    const $li = $(`<li>
+      <a href="${node.url}">
+        <div class="site">
+          <div class="logo">${node.logo[0]}</div>
+          <div class="link">${simplifyUrl(node.url)}</div>
+          <div class="close">
+          <svg class="icon">
+    <use xlink:href="#icon-close"></use>
+</svg></div>
+        </div>
+      </a>
+      <div>
+    </li>`).insertBefore($lastLi)
+    $li.on('click', '.close', (e) => {
+      e.stopPropagation()
+    })
+  })
+}
+
+render()
+
 $('.addButton')
   .on('click', () => {
     let url = window.prompt('请问你要添加的网址是啥？')
     if (url.indexOf('http') !== 0) {
       url = 'https://' + url
     }
-    const $siteList = $('.siteList')
-    const $lastLi = $siteList.find('li.addButton')
-    const $li = $(`<li>
-    <a href="${url}">
-      <div class="site">
-        <div class="logo">${url[0]}</div>
-        <div class="link">${url}</div>
-      </div>
-    </a>
-    <div>
-  </li>`).insertBefore($lastLi)
+    hashMap.push({
+      logo: simplifyUrl(url)[0],
+      url: url
+    })
+    render()
   })
+
+window.onbeforeunload = () => {
+  console.log('页面要关闭了');
+  const string = JSON.stringify(hashMap)
+  localStorage.setItem('x', string)
+}
